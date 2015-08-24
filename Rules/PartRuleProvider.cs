aef251fcee107ffc8df2e30796a14381bbd67ec1
neTp9c <alexpetryakov@gmail.com>
2015-08-24 16:09:34 +0300

    Initial commit

---
+++ Rules/PartRuleProvider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using MainBit.Utility.Services;
+using Orchard.Widgets.Services;
+using Orchard.Environment.Extensions;
+
+namespace MainBit.Utility.Rules
+{
+    [OrchardFeature("MainBit.Utility.Rules")]
+    public class PartRuleProvider : IRuleProvider {
+        private readonly ICurrentContentAccessor _currentContentAccessor;
+
+        public PartRuleProvider(
+            ICurrentContentAccessor currentContentAccessor)
+        {
+            _currentContentAccessor = currentContentAccessor;
+        }
+
+        public void Process(RuleContext ruleContext) {
+
+            if (!String.Equals(ruleContext.FunctionName, "part", StringComparison.OrdinalIgnoreCase))
+                return;
+
+            var contentItem = _currentContentAccessor.CurrentContentItem;
+            if (contentItem == null)
+            {
+                ruleContext.Result = true;
+                return;
+            }
+
+            var part = Convert.ToString(ruleContext.Arguments[0]);
+            ruleContext.Result = contentItem.Parts.Any(p => p.PartDefinition.Name == part);
+        }
+    }
+}
---
+++ Services/ICurrentContentAccessor.cs
@@ -0,0 +1,59 @@
+﻿// http://skywalkersoftwaredevelopment.net/blog/getting-the-current-content-item-in-orchard
+
+using System.Linq;
+using System.Web.Routing;
+using Orchard;
+using Orchard.ContentManagement;
+using Orchard.Core.Common.Utilities;
+using System.Collections.Generic;
+
+namespace MainBit.Utility.Services {
+	public interface ICurrentContentAccessor : IDependency {
+		ContentItem CurrentContentItem { get; }
+	}
+
+
+	public class CurrentContentAccessor : ICurrentContentAccessor {
+		private readonly IContentManager _contentManager;
+		private readonly RequestContext _requestContext;
+
+		public CurrentContentAccessor(IContentManager contentManager, RequestContext requestContext) {
+			_contentManager = contentManager;
+			_requestContext = requestContext;
+		}
+
+        private ContentItem _currentContentItem = null;
+		public ContentItem CurrentContentItem {
+			get {
+                if (_currentContentItem == null) {
+                    _currentContentItem = GetCurrentContentItem();
+                }
+                return _currentContentItem; 
+            }
+		}
+
+        private List<string> _idKeys = new List<string> { "id", "blogId" };
+        public virtual List<string> IdKeys
+        {
+            get { return _idKeys; }
+        }
+
+		private ContentItem GetCurrentContentItem() {
+			var contentId = GetCurrentContentItemId();
+			return contentId == null ? null : _contentManager.Get(contentId.Value);
+		}
+
+		private int? GetCurrentContentItemId() {
+
+			object id = null;
+            if (IdKeys.Any(idKey => _requestContext.RouteData.Values.TryGetValue(idKey, out id)))
+            {
+                int contentId;
+                if (int.TryParse(id as string, out contentId))
+                    return contentId;
+            }
+
+		    return null;
+		}
+	}
+}
