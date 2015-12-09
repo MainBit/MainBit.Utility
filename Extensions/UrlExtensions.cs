bc0bba22d6c7b6b133c2d405bc551b3272595ba7
neTp9c <alexpetryakov@gmail.com>
2015-12-09 14:32:12 +0300

    Add url extensions

---
+++ Extensions/UrlExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Orchard.ContentManagement;
+using System.Web;
+using System.Web.Mvc;
+using Orchard.Mvc.Html;
+
+namespace MainBit.Utility.Extensions
+{
+    public static class UrlExtensions
+    {
+        public static string ItemDisplayUrlAbsolute(this UrlHelper urlHelper, IContent content)
+        {
+            string relativeUrl = urlHelper.ItemDisplayUrl(content);
+
+            var request = HttpContext.Current.Request;
+            return string.Format("http{0}://{1}{2}",
+                request.IsSecureConnection ? "s" : "",
+                request.Url.Host,
+                relativeUrl
+            );
+        }
+
+        public static string ConvertToAbsoluteUrl(this UrlHelper urlHelper, string url)
+        {
+            if (url.StartsWith("http://") || url.StartsWith("https://"))
+            {
+                return url;
+            }
+
+            var request = urlHelper.RequestContext.HttpContext.Request;
+
+            //return string.Format("{0}://{1}{2}",
+            //                   requestUrl.Scheme,
+            //                   requestUrl.Authority,
+            //                   VirtualPathUtility.ToAbsolute(virtualPath));
+
+            return string.Format("http{0}://{1}{2}",
+                request.IsSecureConnection ? "s" : "",
+                request.Url.Host,
+                VirtualPathUtility.ToAbsolute(url)
+            );
+        }
+    }
+}
