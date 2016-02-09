734903fc9625519235dd38e83a12fdfa41346926
neTp9c <alexpetryakov@gmail.com>
2016-02-09 23:07:04 +0300

    Fix for 1.10 compatibility

--- Extensions/HtmlExtensions.cs
+++ Extensions/HtmlExtensions.cs
@@ -1,10 +1,6 @@
 ﻿using MainBit.Utility.Services;
 using Orchard.ContentManagement;
 using Orchard.Mvc.Html;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace MainBit.Utility.Extensions
